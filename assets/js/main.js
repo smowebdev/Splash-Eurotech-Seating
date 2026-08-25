eabab2a8b4c9b6f04a6378806b80810c68bb0ef4
smowebdev <smowebdev@gmail.com>
2026-08-25 09:43:00 +0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+let lenis = null;
+
+if (typeof Lenis !== "undefined") {
+  lenis = new Lenis({
+    duration: 1.2,
+    smoothWheel: true,
+    smoothTouch: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
 $(function () {
   // Slider Explore Collections In Home Page - Start
   $("section")
